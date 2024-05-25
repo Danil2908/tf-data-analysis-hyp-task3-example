eb6b8115ad03ffbdaf835b98c0a434871a039e2d
Danil2908 <danil_ib@mail.ru>
2024-05-25 21:09:53 +0300

    solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1527061415 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control_npv: np.array, test_npv: np.array) -> bool: 
+    mean_control = sum(control_npv) / len(control_npv)
+    mean_test = sum(test_npv) / len(test_npv)
+    
+    # t тестт для сравнения средних двух выборок 
+    z_score, p_value = stats.ttest_ind(control_npv, test_npv)
+    
+    alpha = 0.03
+    
+    return p_value < alpha
